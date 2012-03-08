e773c35754c17dd22eb7a41dd9e3210bce344b87
Kapil Dalwani <kapildalwani@gmail.com>
2012-03-08 13:57:35 -0800

    Adding more ways

--- src/GenerateSets.java
+++ src/GenerateSets.java
@@ -5,11 +5,17 @@ public class GenerateSets {
 	
 	public static void main(String argv[]){
 		
-		generateSetsRecursive("", "ABC");
-		generateSetsBitWise("ABC");
+//		generateSetsRecursive("", "ABC");
+//		generateSetsBitWise("ABC");
+//		generateRecurviseSets(0, "", "ABC");
+		generateRecurviseSetsofSize(0, 3, "", "ABCDE");
 	}
 	
 	
+	/*
+	 * The typical exclusion inclusion way of subsets
+	 * At each call either you add the first element or you don't to the final string
+	 */
 	public static void generateSetsRecursive(String left, String remaining){
 		if(remaining.length() == 0){
 			System.out.println(left);
@@ -18,10 +24,12 @@ public class GenerateSets {
 		
 		generateSetsRecursive(left + "", remaining.substring(1));
 		generateSetsRecursive(left + remaining.substring(0, 1), remaining.substring(1));
-		
 	}
 	
-	
+	/*
+	 * The way to generate all subsets is to iterate over all binary numbers for that length of that string
+	 * and use the and property to find exclusion/inclusion.
+	 */
 	public static void generateSetsBitWise(String input){
 		
 		
@@ -39,4 +47,38 @@ public class GenerateSets {
 		}
 		
 	}
+	
+	/*
+	 * Again, the same inclusion exclusion way but in this case we are using index.
+	 * start at zero, include or exclude it, then move to the next index of string and then do the 
+	 * same logic.
+	 */
+	public static void generateRecurviseSets(int start, String soFar, String org){
+		
+		if(start == org.length()){
+			System.out.println(soFar);
+			return;
+		}
+			
+		generateRecurviseSets(start+1, soFar + org.substring(start,start+1), org);
+		generateRecurviseSets(start+1, soFar , org);
+	}
+	
+	/*
+	 * This generates teh subsets of size left.
+	 * At every step we have k options out of org.length() - star left.
+	 */
+	public static void generateRecurviseSetsofSize(int start, int left , String soFar, String org){
+		
+		if(left == 0){
+			System.out.println(soFar);
+			return;
+		}
+			
+		for(int i = start; i < org.length();i++){
+			String newsoFar = soFar + org.substring(i,i+1);
+			generateRecurviseSetsofSize(i+1, left-1, newsoFar, org);
+		}
+	}
+	
 }
